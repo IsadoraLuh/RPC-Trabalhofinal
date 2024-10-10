d3642afcca6fc3ee7ded5d35502692857a4e7808
IsadoraLuh <161628180+IsadoraLuh@users.noreply.github.com>
2024-10-10 09:09:36 -0300

    \

---
+++ RPC-Trabalhofinal/Assets/Scripts/TankShell.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankShell : MonoBehaviour
+{
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.up;
+    }
+
+  
+    void Update()
+    {
+        
+    }
+}
